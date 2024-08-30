7f6d2971972d126d512d90eea3f31d972e0a9ca0
Rohit544 <monuoo1009@gmail.com>
2024-08-30 08:58:32 +0530

    add for in loop

--- hello.js
+++ hello.js
@@ -8,3 +8,22 @@ console.log(a)
 
 a.push("dream car")
 console.log(a)
+
+
+// var s = 5
+// var n = 10
+
+// do{
+//     console.log(s)
+// }while (i<=s)
+
+
+var colors = {
+    primery : "green",
+    secondry : "blue",
+    lastColor : "while"
+}
+
+for(color in colors){
+    console.log(color+ ' -> ' + colors[color])
+}
